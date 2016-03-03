29f41149a4a3e0ef159075b5dc677d2d1c13391d
edouard puillandre <edouard.puillandre@epitech.eu>
2016-03-03 11:25:21 +0100

    deff all struct

--- edouard/include/tetris.h
+++ edouard/include/tetris.h
@@ -5,13 +5,67 @@
 ** Login   <puilla_e@epitech.net>
 ** 
 ** Started on  Thu Feb 25 18:47:38 2016 edouard puillandre
-** Last update Thu Feb 25 19:27:21 2016 edouard puillandre
+** Last update Thu Mar  3 11:24:38 2016 edouard puillandre
 */
 
 #ifndef		TETRIS_H_
 # define	TETRIS_H_
 
+# include <ncurses/curses.h>
+# include <stdlib.h>
+# include <stdbool.h>
+# include <unistd.h>
 # include "my.h"
 # define PATH_GAME "./ressource/game"
+# define TETRIMINO "./tetriminos"
+
+typedef struct	s_cmd
+{
+  int		kl;
+  int		kr;
+  int		k_turn;
+  int		k_drop;
+  int		k_quit;
+  int		k_pause;
+}		t_cmd;
+
+typedef struct	s_map
+{
+  int		width;
+  int		height;
+  char		**tab;
+}		t_map;
+
+typedef struct	s_piece
+{
+  int		width;
+  int		height;
+  int		col;
+  char		**shape;
+}		t_piece;
+
+typedef struct	s_clock
+{
+  int		min;
+  int		sec;
+}		t_clock;
+
+typedef struct	s_game
+{
+  bool		next;
+  int		lvl;
+  t_clock	t;
+  int		score;
+  int		high_score;
+}		t_game;
+
+typedef struct	s_tetris
+{
+  t_cmd		cmd;
+  t_map		map;
+  t_game	game;
+  int		n_piece;
+  t_piece	*piece;
+}		t_tetris;
 
 #endif /* !TETRIS_H_ */
--- edouard/src/main.c
+++ edouard/src/main.c
@@ -5,12 +5,21 @@
 ** Login   <puilla_e@epitech.net>
 ** 
 ** Started on  Thu Feb 25 18:46:48 2016 edouard puillandre
-** Last update Thu Feb 25 18:47:23 2016 edouard puillandre
+** Last update Thu Mar  3 11:24:56 2016 edouard puillandre
 */
 
 #include "tetris.h"
 
-int	main()
+int	main(int argc, char **argv)
 {
+  bool	n;
+  (void) argc;
+  (void) argv;
+
+  n = false;
+  if (n)
+    write(1, "yes", 3);
+  else
+    write(1, "no", 2);
   return (0);
 }
