5a891934ddd22c3151ba2402500a7f7ab45cf170
edouard puillandre <edouard.puillandre@epitech.eu>
2016-03-11 16:20:06 +0100

    proto fct_list

--- src/fct_list.c
+++ src/fct_list.c
@@ -5,14 +5,32 @@
 ** Login   <puilla_e@epitech.net>
 ** 
 ** Started on  Fri Mar 11 10:43:08 2016 edouard puillandre
-** Last update Fri Mar 11 10:53:39 2016 edouard puillandre
+** Last update Fri Mar 11 15:58:28 2016 edouard puillandre
 */
 
 #include "tetris.h"
 
-int		add_elem(t_piece *)
+int	add_elem(t_piece *elem, t_piece *list)
 {
-  t_piece	*elem;
+  return (0);
+}
 
-  if (
+int	swap_elemn(t_piece *elem, t_piece *list)
+{
+  return (0);
+}
+
+int	rm_elem(t_piece *elem, t_piece *list);
+{
+  return (0);
+}
+
+int	sort_list(t_piece *list)
+{
+  return (0);
+}
+
+int	rm_error(t_piece *list)
+{
+  return (0);
 }
